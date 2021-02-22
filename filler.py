d6788878186069fa5cb06092a5da350fb3a8df46
Peter <pebueku@gmail.com>
2021-02-23 00:22:33 +0100

    script now scrapes moodle for the input it needs

--- filler.py
+++ filler.py
@@ -30,10 +30,10 @@ def main():
     main
     '''
 
-    moodle_login('Enter moodle_username here', 'Enter moodle Password here') # Enter your username and password for the moodle sites as a string
+    moodle_login(os.environ.get('MOODLE_USERNAME'), os.environ.get('MOODLE_PASSWORD')) # Enter your username and password for the moodle sites as a string
 
-    course_url = 'https://moodle.covenantuniversity.edu.ng/mod/feedback/complete.php?id=43258&courseid='
-
+    course_url = 'https://moodle.covenantuniversity.edu.ng/mod/feedback/complete.php?id=43637&courseid='
+    
 
     # Gets course_id for each course you are registered for 
     elements = driver.find_elements_by_class_name('aalink')
@@ -46,89 +46,37 @@ def main():
             continue
         course_id.append(item)
 
-        if '43258' in course_id:
-            course_id.remove('43258')
+        if '43637' in course_id:
+            course_id.remove('43637')
 
-    
+    driver.get(course_url+course_id[0])
+    values = [2,3,3,2,2,3,3,2,2,2,2,3,3,3,2,3,3,2,2,3,2,3,3,2,3,3,2,3,2,3]
+ 
+        
+
 
     # Loops through course via course id
     for id in course_id:
         driver.get(course_url+id)
-
-        # Checks to see if evaluation for this course has already been filled 
-        try:
-            select  = Select(driver.find_element_by_id('id_multichoice_306'))
+        index = 0
+        selectors = driver.find_elements_by_class_name('custom-select')
+        for element in selectors:
+            element_id = element.get_attribute('id')
+            select = Select(driver.find_element_by_id(element_id))
+            try:
+                select.select_by_index(values[index])
+            except:
+                break
+            index+=1
+        try:    
+            lecturer_name = driver.find_element_by_id('id_textfield_390')
         except:
             continue
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_307'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_309'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_310'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_312'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_313'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_336'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_314'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_337'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_316'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_317'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_318'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_320'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_338'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_321'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_339'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_324'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_323'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_326'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_327'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_329'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_340'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_330'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_342'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_343'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_345'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_346'))
-        select.select_by_visible_text('Agree')
-        select  = Select(driver.find_element_by_id('id_multichoice_332'))
-        select.select_by_visible_text('Disagree')
-        select  = Select(driver.find_element_by_id('id_multichoice_333'))
-        select.select_by_visible_text('Agree')
-
-        lecturer_name = driver.find_element_by_id('id_textfield_347')
         lecturer_name.send_keys("Null")
+        driver.find_element_by_id('id_savevalues').click()
 
-        select  = Select(driver.find_element_by_id('id_multichoice_335'))
-        select.select_by_visible_text('Good')
-
-        submit = driver.find_element_by_id('id_savevalues')
-        submit.click()
         
-
-
+   
 
 
 
